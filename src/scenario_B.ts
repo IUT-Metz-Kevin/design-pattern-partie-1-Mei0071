3eb4b62db6a6a2a53a3406a6ede8029b6a346c57
Ho Mei-Yi <mei-yi.ho9@etu.univ-lorraine.fr>
2025-04-23 10:45:22 +0200

    essai début sce B

--- src/scenario_B.ts
+++ src/scenario_B.ts
@@ -1,1 +1,46 @@
 //Composite 
+interface Entreprise{
+    afficher(affichage:string):void;
+}
+
+class SousDepartement implements Entreprise{
+    private _name:string;
+    private _employes:Employes[];
+
+    constructor(name:string){
+        this._name=name;
+    }
+
+    afficher(affichage: string): void {
+        console.log("Sous departement : "+this._name);
+    }
+
+    addEmployes(employes: Employes): void {
+        this._employes.push(employes);
+    }
+}
+
+class Departement implements Entreprise{
+    private _name:string;
+    private _sousDep:Entreprise[];
+    private _employes:Employes[];
+
+    constructor(name:string){
+        this._name=name;
+        this._sousDep=[];
+        this._employes=[];
+    }
+
+    afficher(affichage: string): void {
+        console.log("Departement : "+this._name);
+       
+    }
+
+    addEmployes(employes: Employes): void {
+        this._employes.push(employes);
+    }
+}
+
+ class Employes implements Entreprise{
+
+ }
