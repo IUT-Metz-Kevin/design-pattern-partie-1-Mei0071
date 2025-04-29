5d35d53d8de59e3f3dab54c55cfb63d37a43d85e
Mei-Yi <mei-yi.ho@orange.fr>
2025-04-29 19:25:39 +0200

    scenar D

--- src/scenario_D.ts
+++ src/scenario_D.ts
@@ -1,4 +1,58 @@
 //Décorateur 
 interface Boisson{
-    description():string;
+    description():void;
+    tarif():number;
 }
+
+class Cafe implements Boisson{
+    description(): string {
+        return "café";
+    }
+
+    tarif(): number {
+        return 4;
+    }
+}
+
+abstract class CafeDecorateur implements Boisson{
+    protected boisson:Boisson;
+
+    constructor(boisson:Boisson){
+        this.boisson=boisson;
+    }
+
+    abstract description(): void;
+
+    abstract tarif(): number
+}
+
+class Lait extends CafeDecorateur{
+    description(): string {
+        return this.boisson.description()+", Lait";
+    }
+
+    tarif(): number {
+        return this.boisson.tarif()+1;
+    }
+}
+
+class Chantilly extends CafeDecorateur{
+    description(): string {
+        return this.boisson.description()+", Chantilly";
+    }
+
+    tarif(): number {
+        return this.boisson.tarif()+1;
+    }
+}
+
+class Vanille extends CafeDecorateur{
+    description(): string {
+        return this.boisson.description()+", Vanille";
+    }
+
+    tarif(): number {
+        return this.boisson.tarif()+0.5;
+    }
+}
+
